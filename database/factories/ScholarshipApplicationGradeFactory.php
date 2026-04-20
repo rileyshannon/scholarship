574447b783f83d2c544511a87df67d0d6f6538f0
Riley Shannon <riley.shannon@icloud.com>
2026-04-20 00:36:45 -0500

    Updated score values

--- database/factories/ScholarshipApplicationGradeFactory.php
+++ database/factories/ScholarshipApplicationGradeFactory.php
@@ -11,41 +11,39 @@ class ScholarshipApplicationGradeFactory extends Factory
 {
     public function definition(): array
     {
-        $grades = collect([
-            'short_term_goal_grade',
-            'long_term_goal_grade',
-            'received_awards_grade',
-            'academics_grade',
-            'other_organizations_grade',
-            'volunteer_events_grade',
-            'career_progression_grade',
-            'essay_one_grade',
-            'essay_two_grade',
-        ])->mapWithKeys(fn ($field) => [$field => fake()->numberBetween(0, 20)]);
+        $shortTermGoal      = fake()->numberBetween(0, 2);
+        $longTermGoal       = fake()->numberBetween(0, 1);
+        $receivedAwards     = fake()->numberBetween(0, 5);
+        $academics          = fake()->numberBetween(0, 4);
+        $otherOrganizations = fake()->numberBetween(0, 5);
+        $volunteerEvents    = fake()->numberBetween(0, 10);
+        $careerProgression  = fake()->numberBetween(0, 10);
+        $essayOne           = fake()->numberBetween(0, 20);
+        $essayTwo           = fake()->numberBetween(0, 20);
 
         return [
-            'scholarship_application_id'  => ScholarshipApplication::factory(),
-            'user_id'                     => User::factory(),
-            'status'                      => GradeStatus::Active->value,
-            'short_term_goal_grade'       => $grades['short_term_goal_grade'],
-            'short_term_goal_comments'    => fake()->sentence(),
-            'long_term_goal_grade'        => $grades['long_term_goal_grade'],
-            'long_term_goal_comments'     => fake()->sentence(),
-            'received_awards_grade'       => $grades['received_awards_grade'],
-            'received_awards_comments'    => fake()->sentence(),
-            'academics_grade'             => $grades['academics_grade'],
-            'academics_comments'          => fake()->sentence(),
-            'other_organizations_grade'   => $grades['other_organizations_grade'],
+            'scholarship_application_id'   => ScholarshipApplication::factory(),
+            'user_id'                      => User::factory(),
+            'status'                       => GradeStatus::Active->value,
+            'short_term_goal_grade'        => $shortTermGoal,
+            'short_term_goal_comments'     => fake()->sentence(),
+            'long_term_goal_grade'         => $longTermGoal,
+            'long_term_goal_comments'      => fake()->sentence(),
+            'received_awards_grade'        => $receivedAwards,
+            'received_awards_comments'     => fake()->sentence(),
+            'academics_grade'              => $academics,
+            'academics_comments'           => fake()->sentence(),
+            'other_organizations_grade'    => $otherOrganizations,
             'other_organizations_comments' => fake()->sentence(),
-            'volunteer_events_grade'      => $grades['volunteer_events_grade'],
-            'volunteer_events_comments'   => fake()->sentence(),
-            'career_progression_grade'    => $grades['career_progression_grade'],
-            'career_progression_comments' => fake()->sentence(),
-            'essay_one_grade'             => $grades['essay_one_grade'],
-            'essay_one_comments'          => fake()->sentence(),
-            'essay_two_grade'             => $grades['essay_two_grade'],
-            'essay_two_comments'          => fake()->sentence(),
-            'final_score'                 => $grades->sum(),
+            'volunteer_events_grade'       => $volunteerEvents,
+            'volunteer_events_comments'    => fake()->sentence(),
+            'career_progression_grade'     => $careerProgression,
+            'career_progression_comments'  => fake()->sentence(),
+            'essay_one_grade'              => $essayOne,
+            'essay_one_comments'           => fake()->sentence(),
+            'essay_two_grade'              => $essayTwo,
+            'essay_two_comments'           => fake()->sentence(),
+            'final_score'                  => $shortTermGoal + $longTermGoal + $receivedAwards + $academics + $otherOrganizations + $volunteerEvents + $careerProgression + $essayOne + $essayTwo,
         ];
     }
 
